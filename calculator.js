079d0463d8caecf44dbb68ba8875a0e978dba347
Renee <rchar20@gmail.com>
2016-09-07 16:41:26 -1000

    Calculator functions added and load and getTotal functions are working

--- calculator.js
+++ calculator.js
@@ -7,66 +7,91 @@
  */
 
 function calculatorModule() {
-    var _memory;
-    var _total;
-    return {
-      memory: _memory,
-      total: _total
-    };
-}
+  let _memory = 0;
+  let _total = 0;
+  
+  return {
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
-
+    load: function(newTotal){
+     _total = newTotal;
+     return _total;
+    },
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
-
+    getTotal: function() {
+      return _total;
+    },
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
-
+    addToTotal: function(valueToAdd) {
+      _total += valueToAdd;
+      return _total;
+    },
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+   subtractFromTotal: function(valueToSubtract) {
+      _total -= valueToSubtract;
+      return _total;
+    },
 
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+   multiplyValue: function(valueToMultiply) {
+      valueToMultiply *= _total;
+      return valueToMultiply;
+    },
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-
+   divideValue: function(valueToDivide) {
+      valueToDivide /= total;
+      return valueToDivide;
+    },
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
+    getMemory: function() {
+      return _memory;
+    },
 
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+    storeTotalInMemory: function(theTotal) {
+      _memory = _total;
+      return _memory;
+    },
 
   /**
    * Clear the value stored at `memory`
    */
-
+    clearMemory: function() {
+      _memory = null;
+      return _memory;
+    }
   /**
    * Validation
    */
-
+  };
+};
